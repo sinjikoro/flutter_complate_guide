72229278f7a83c5b7a1ee6365c08670e18b7a913
sinjikoro <shinnaga.dev@gmail.com>
2022-12-16 00:52:59 +0900

    44.Passing Callback Functions Around

---
+++ lib/answer.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Answer extends StatelessWidget {
+  final Function selectHandler;
+
+  Answer(this.selectHandler);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: double.infinity,
+        child: ElevatedButton(
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(Colors.blue),
+            ),
+            onPressed: selectHandler,
+            child: Text('Answer 1')));
+  }
+}
