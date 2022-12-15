06090cc9c3fa7847fde873d704245f28cdae173f
sinjikoro <shinnaga.dev@gmail.com>
2022-12-15 12:01:53 +0900

    32.Connecting Functions & Buttons

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key key}) : super(key: key);
+  void answerQuestion() {
+    print('Answer chosen!');
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,9 +18,9 @@ class MyApp extends StatelessWidget {
         home: Scaffold(
       appBar: AppBar(title: Text('The Question!')),
       body: Column(children: <Widget>[
-        ElevatedButton(child: Text('Answer 1'), onPressed: null),
-        ElevatedButton(child: Text('Answer 2'), onPressed: null),
-        ElevatedButton(child: Text('Answer 3'), onPressed: null),
+        ElevatedButton(child: Text('Answer 1'), onPressed: answerQuestion),
+        ElevatedButton(child: Text('Answer 2'), onPressed: answerQuestion),
+        ElevatedButton(child: Text('Answer 3'), onPressed: answerQuestion),
       ]),
     ));
   }
