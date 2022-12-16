ef547ec0b9fe041afa45543f16f83955768b89c9
sinjikoro <shinnaga.dev@gmail.com>
2022-12-17 01:36:14 +0900

    55.Resetting the Quiz

--- lib/main.dart
+++ lib/main.dart
@@ -14,24 +14,50 @@ class _MyAppState extends State<MyApp> {
   final questions = const [
     {
       'questionText': 'Whar\'s your favorite color?',
-      'answers': ['White', 'Red', 'Blue', 'Green']
+      'answers': [
+        {'text': 'White', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Blue', 'score': 3},
+        {'text': 'Green', 'score': 1},
+      ]
     },
     {
       'questionText': 'Whar\'s your favorite animal?',
-      'answers': ['Cat', 'Dog', 'Fish', 'Lion']
+      'answers': [
+        {'text': 'Cat', 'score': 3},
+        {'text': 'Dog', 'score': 10},
+        {'text': 'Fish', 'score': 1},
+        {'text': 'Lion', 'score': 5},
+      ]
     },
     {
       'questionText': 'Whar\'s your favorite Team?',
-      'answers': ['Giants', 'Hanshin', 'NipponHum', 'BayStars']
+      'answers': [
+        {'text': 'Giants', 'score': 1},
+        {'text': 'Hanshin', 'score': 10},
+        {'text': 'NipponHum', 'score': 5},
+        {'text': 'BayStars', 'score': 3},
+      ]
     }
   ];
 
   var _questionIndex = 0;
-  void _answerQuestion() {
+  var _totalScore = 0;
+
+  void _answerQuestion(int score) {
+    _totalScore += score;
+
     setState(() {
       _questionIndex++;
     });
     print('Answer chosen!');
+  }
+
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
   }
 
   @override
@@ -45,7 +71,7 @@ class _MyAppState extends State<MyApp> {
               answerQuestion: _answerQuestion,
               questionIndex: _questionIndex,
             )
-          : Result(),
+          : Result(_totalScore, _resetQuiz),
     ));
   }
 }
--- lib/quiz.dart
+++ lib/quiz.dart
@@ -17,9 +17,10 @@ class Quiz extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(children: <Widget>[
       Question(questions[questionIndex]['questionText']),
-      ...(questions[questionIndex]['answers'] as List<String>).map(((answer) {
-        return Answer(answerQuestion, answer);
-      }))
+      ...(questions[questionIndex]['answers'] as List<Map<String, Object>>)
+          .map(((answer) {
+        return Answer(() => answerQuestion(answer['score']), answer['text']);
+      })).toList()
     ]);
   }
 }
