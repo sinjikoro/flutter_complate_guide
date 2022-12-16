832a09eb2685c9013dd60815d875272068eb6a5b
sinjikoro <shinnaga.dev@gmail.com>
2022-12-17 00:25:33 +0900

    51.Outputting Widgets Conditionally

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,21 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final questions = const [
+    {
+      'questionText': 'Whar\'s your favorite color?',
+      'answers': ['White', 'Red', 'Blue', 'Green']
+    },
+    {
+      'questionText': 'Whar\'s your favorite animal?',
+      'answers': ['Cat', 'Dog', 'Fish', 'Lion']
+    },
+    {
+      'questionText': 'Whar\'s your favorite Team?',
+      'answers': ['Giants', 'Hanshin', 'NipponHum', 'BayStars']
+    }
+  ];
+
   var _questionIndex = 0;
   void _answerQuestion() {
     setState(() {
@@ -20,31 +35,18 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      {
-        'questionText': 'Whar\'s your favorite color?',
-        'answers': ['White', 'Red', 'Blue', 'Green']
-      },
-      {
-        'questionText': 'Whar\'s your favorite animal?',
-        'answers': ['Cat', 'Dog', 'Fish', 'Lion']
-      },
-      {
-        'questionText': 'Whar\'s your favorite Team?',
-        'answers': ['Giants', 'Hanshin', 'NipponHum', 'BayStars']
-      }
-    ];
-
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(title: Text('The Question!')),
-      body: Column(children: <Widget>[
-        Question(questions[_questionIndex]['questionText']),
-        ...(questions[_questionIndex]['answers'] as List<String>)
-            .map(((answer) {
-          return Answer(_answerQuestion, answer);
-        }))
-      ]),
+      body: _questionIndex < questions.length
+          ? Column(children: <Widget>[
+              Question(questions[_questionIndex]['questionText']),
+              ...(questions[_questionIndex]['answers'] as List<String>)
+                  .map(((answer) {
+                return Answer(_answerQuestion, answer);
+              }))
+            ])
+          : Center(child: Text('You did it!')),
     ));
   }
 }
